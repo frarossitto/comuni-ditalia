75713f472be96057fd6f800780aaad5fa349626d
frarossitto <rossitto.francesco001@gmail.com>
2026-09-14 22:25:06 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-/*
-  COMUNI D'ITALIA 🇮🇹
-
-  File riservato alle funzioni generali
-  che aggiungeremo in futuro.
-
-  La struttura è stata lasciata separata
-  per permettere al sito di crescere.
-*/
