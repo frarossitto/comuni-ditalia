2697c71397c5b495219088e8def1c409c3bb432e
frarossitto <rossitto.francesco001@gmail.com>
2026-09-14 23:03:02 +0200

    Create comuni.js

---
+++ comuni.js
@@ -0,0 +1,21 @@
+window.COMUNI_DATA = {
+
+  "108040": {
+    nome: "Seveso",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/09/2026",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108019": {
+    nome: "Cesano Maderno",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "",
+    descrizione: "",
+    foto: ""
+  }
+
+};
