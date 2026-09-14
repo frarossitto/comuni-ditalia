98436f035162fce411f36c6ac35ed3c6c1e93676
frarossitto <rossitto.francesco001@gmail.com>
2026-09-14 22:38:30 +0200

    Create comuni.js

---
+++ comuni.js
@@ -0,0 +1,20 @@
+window.COMUNI_DATA = {
+
+  "108040": {
+
+    nome: "Seveso",
+
+    provincia: "Monza e Brianza",
+
+    regione: "Lombardia",
+
+    data: "14/09/2026",
+
+    descrizione:
+      "Prima tappa del viaggio attraverso tutti i comuni d’Italia.",
+
+    foto: ""
+
+  }
+
+};
