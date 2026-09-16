1ac4e2044eff5023f5dc1b1b2005e3c069a39d47
frarossitto <rossitto.francesco001@gmail.com>
2026-09-16 20:34:05 +0200

    Update visitati.js

--- visitati.js
+++ visitati.js
@@ -1,8 +1,95 @@
-// INSERISCI QUI I CODICI ISTAT DEI COMUNI VISITATI.
-// NON MODIFICARE ALTRO.
+// AGGIORNA SOLO QUESTO ELENCO.
+// Inserisci i CODICI ISTAT dei comuni che hai visitato.
 
-window.VISITED_ISTAT = [
+const VISITED_ISTAT = [
 
-  "108040"
+  // 08 MAGGIO 2025
+  "108054", // Lentate sul Seveso
+  "108025", // Lazzate
+  "108032", // Misinto
+  "108020", // Cogliate
+
+  // 09 MAGGIO 2025
+  "108005", // Barlassina
+  "108040", // Seveso
+  "108019", // Cesano Maderno
+
+  // 10 MAGGIO 2025
+  "108030", // Meda
+  "108039", // Seregno
+
+  // 11 MAGGIO 2025
+  "108027", // Limbiate
+  "108010", // Bovisio Masciago
+  "108023", // Desio
+
+  // 14 MAGGIO 2025
+  "108018", // Ceriano Laghetto
+
+  // 17 MAGGIO 2025
+  "108024", // Giussano
+  "108048", // Verano Brianza
+  "108015", // Carate Brianza
+  "108008", // Besana Brianza
+
+  // 19 MAGGIO 2025
+  "013201", // Rovellasca
+  "013028", // Bregnano
+  "013064", // Cermenate
+
+  // 26 MAGGIO 2025
+  "108045", // Varedo
+  "108035", // Nova Milanese
+  "013202", // Rovello Porro
+
+  // 28 MAGGIO 2025
+  "013101", // Figino Serenza
+  "013163", // Novedrate
+  "013143", // Mariano Comense
+  "013035", // Cabiate
+
+  // 04 GIUGNO 2025
+  "013046", // Carimate
+  "013242", // Vertemate con Minoprio
+  "013036", // Cadorago
+
+  // 07 GIUGNO 2025
+  "108011", // Briosco
+  "108037", // Renate
+  "108043", // Triuggio
+  "108003", // Albiate
+  "108041", // Sovico
+  "108029", // Macherio
+  "108009", // Biassono
+  "108046", // Vedano al Lambro
+  "108049", // Villasanta
+  "108034", // Muggio
+
+  // 14 GIUGNO 2025
+  "108028", // Lissone
+  "108012", // Brugherio
+  "108021", // Concorezzo
+  "108004", // Arcore
+  "108026", // Lesmo
+  "108014", // Camparada
+  "108044", // Usmate Velate
+
+  // 22 GIUGNO 2025
+  "108038", // Ronco Briantino
+  "108016", // Carnate
+  "108007", // Bernareggio
+  "108050", // Vimercate
+  "108013", // Burago di Molgora
+  "108052", // Caponago
+  "108036", // Ornago
+
+  // 28 GIUGNO 2025
+  "108002", // Aicurzio
+  "108042", // Sulbiate
+  "108006", // Bellusco
+  "108051", // Busnago
+  "108031", // Mezzago
+  "108055", // Roncello
+  "108017"  // Cavenago di Brianza
 
 ];
