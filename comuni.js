510948cdf705b2c4282aa27f106e24a9ebae0f81
frarossitto <rossitto.francesco001@gmail.com>
2026-09-16 20:34:38 +0200

    Update comuni.js

--- comuni.js
+++ comuni.js
@@ -1,18 +1,552 @@
 window.COMUNI_DATA = {
 
-  "108040": {
-
-    nome: "Seveso",
-
+  "108054": {
+    nome: "Lentate sul Seveso",
     provincia: "Monza e Brianza",
-
     regione: "Lombardia",
+    data: "08/05/2025",
+    descrizione: "",
+    foto: ""
+  },
 
-    data: "14/09/2026",
+  "108025": {
+    nome: "Lazzate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "08/05/2025",
+    descrizione: "",
+    foto: ""
+  },
 
-    descrizione:
-      "Il primo comune del viaggio."
+  "108032": {
+    nome: "Misinto",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "08/05/2025",
+    descrizione: "",
+    foto: ""
+  },
 
+  "108020": {
+    nome: "Cogliate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "08/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108005": {
+    nome: "Barlassina",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "09/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108040": {
+    nome: "Seveso",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "09/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108019": {
+    nome: "Cesano Maderno",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "09/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108030": {
+    nome: "Meda",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "10/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108039": {
+    nome: "Seregno",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "10/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108027": {
+    nome: "Limbiate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "11/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108010": {
+    nome: "Bovisio Masciago",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "11/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108023": {
+    nome: "Desio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "11/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108018": {
+    nome: "Ceriano Laghetto",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108024": {
+    nome: "Giussano",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "17/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108048": {
+    nome: "Verano Brianza",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "17/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108015": {
+    nome: "Carate Brianza",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "17/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108008": {
+    nome: "Besana Brianza",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "17/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013201": {
+    nome: "Rovellasca",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "19/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013028": {
+    nome: "Bregnano",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "19/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013064": {
+    nome: "Cermenate",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "19/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108045": {
+    nome: "Varedo",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "26/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108035": {
+    nome: "Nova Milanese",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "26/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013202": {
+    nome: "Rovello Porro",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "26/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013101": {
+    nome: "Figino Serenza",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "28/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013163": {
+    nome: "Novedrate",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "28/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013143": {
+    nome: "Mariano Comense",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "28/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013035": {
+    nome: "Cabiate",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "28/05/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013046": {
+    nome: "Carimate",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "04/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013242": {
+    nome: "Vertemate con Minoprio",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "04/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "013036": {
+    nome: "Cadorago",
+    provincia: "Como",
+    regione: "Lombardia",
+    data: "04/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108011": {
+    nome: "Briosco",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108037": {
+    nome: "Renate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108043": {
+    nome: "Triuggio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108003": {
+    nome: "Albiate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108041": {
+    nome: "Sovico",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108029": {
+    nome: "Macherio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108009": {
+    nome: "Biassono",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108046": {
+    nome: "Vedano al Lambro",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108049": {
+    nome: "Villasanta",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108034": {
+    nome: "Muggiò",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "07/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108028": {
+    nome: "Lissone",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108012": {
+    nome: "Brugherio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108021": {
+    nome: "Concorezzo",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108004": {
+    nome: "Arcore",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108026": {
+    nome: "Lesmo",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108014": {
+    nome: "Camparada",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108044": {
+    nome: "Usmate Velate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "14/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108038": {
+    nome: "Ronco Briantino",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108016": {
+    nome: "Carnate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108007": {
+    nome: "Bernareggio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108050": {
+    nome: "Vimercate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108013": {
+    nome: "Burago di Molgora",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108052": {
+    nome: "Caponago",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108036": {
+    nome: "Ornago",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "22/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108002": {
+    nome: "Aicurzio",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108042": {
+    nome: "Sulbiate",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108006": {
+    nome: "Bellusco",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108051": {
+    nome: "Busnago",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108031": {
+    nome: "Mezzago",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108055": {
+    nome: "Roncello",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
+  },
+
+  "108017": {
+    nome: "Cavenago di Brianza",
+    provincia: "Monza e Brianza",
+    regione: "Lombardia",
+    data: "28/06/2025",
+    descrizione: "",
+    foto: ""
   }
 
 };
